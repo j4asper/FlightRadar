8ef6a0207b637986caaa8fb2211fc66d92c015a9
Jasper <jasper@jazper.dk>
2025-03-31 21:01:15 +0200

    Fix namespace

--- FlightRadar.Client/Models/V1/UsageResponseModel.cs
+++ FlightRadar.Client/Models/V1/UsageResponseModel.cs
@@ -1,6 +1,7 @@
 using System.Text.Json.Serialization;
+using FlightRadar.Entities.Models.V1;
 
-namespace FlightRadar.Entities.Models.V1;
+namespace FlightRadar.Client.Models.V1;
 
 public class UsageResponseModel
 {
